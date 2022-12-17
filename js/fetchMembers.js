407e876a774568d0a4c21faca9948bd351bc16c2
Dániel Szabó <kingalistairlol@gmail.com>
2022-12-17 16:56:26 +0100

    Member edit works

--- js/fetchMembers.js
+++ js/fetchMembers.js
@@ -1,23 +1,21 @@
 const table = document.querySelector('.table')
-const addMemberButton = document.querySelector('.add-member-btn')
-const firstNameTag = document.getElementById('firstName')
-const lastNameTag = document.getElementById('lastName')
-const streetTag = document.getElementById('street')
+
+
+const firstName = document.querySelector('#firstName')
+const lastName = document.querySelector('#lastName')
+const street = document.querySelector('#street')
+const addMemberButton = document.querySelector('#add-member-btn')
+
+
+const editForm = document.querySelector('#editForm')
+const idToEdit = document.querySelector('#id-to-edit')
+const editMemberButton = document.querySelector('#edit-member-btn')
+const editFirstName = document.querySelector('#editFirstName')
+const editLastName = document.querySelector('#editLastName')
+const editStreet = document.querySelector('#editStreet')
+const submitEdit = document.querySelector('#submit-edited-member-btn')
 
 const url = 'http://localhost:8080/members'
-
-let firstName = ''
-let lastName = ''
-let street = ''
-firstNameTag.addEventListener("change", (event) => {
-    firstName = event.target.value
-});
-lastNameTag.addEventListener("change", (event) => {
-    lastName = event.target.value
-});
-streetTag.addEventListener("change", (event) => {
-    street = event.target.value
-});
 
 
 getMembers()
@@ -26,7 +24,6 @@ function getMembers() {
     fetch(url)
         .then((Response) => Response.json())
         .then((members) => {
-            console.log(members)
 
             members.forEach((member) => {
 
@@ -43,7 +40,6 @@ function getMembers() {
                 const tdDeleteButton = document.createElement('button')
                 tdDeleteButton.innerHTML = 'Delete'
 
-
                 tdDeleteButton.addEventListener('click', () => {
 
                     fetch(url + '/' + member.id, {
@@ -59,30 +55,64 @@ function getMembers() {
                         .catch(err => console.log(err))
                 })
 
-                tableRow.append(tdId, tdFirstName, tdLastName, tdStreet, tdDeleteButton);
-                table.appendChild(tableRow);
-            });
-        });
+                tableRow.append(tdId, tdFirstName, tdLastName, tdStreet, tdDeleteButton,)
+                table.appendChild(tableRow)
+            })
+        })
 }
 
 
 addMemberButton.addEventListener('click', () => {
 
-            fetch(url, {
+        fetch(url, {
 
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    firstName: firstName,
-                    lastName: lastName,
-                    street: street
-                }),
-            })
-                .then((response) => response.json())
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstName: firstName.value,
+                lastName: lastName.value,
+                street: street.value
+            }),
+        })
+            .then((response) => response.json())
 
+        alert("New member was added!")
 
     }
 )
+
+editMemberButton.addEventListener('click', () => {
+    editForm.className = 'visible'
+    fetchEdit()
+
+    submitEdit.addEventListener('click',editMember)
+
+})
+
+async function fetchEdit() {
+    const response = await fetch(url + '/' +idToEdit.value).then(response => response.json());
+    editFirstName.value = response.firstName
+    editLastName.value = response.lastName
+    editStreet.value = response.street
+}
+
+async function editMember() {
+
+    fetch(url + '/' + idToEdit.value, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": idToEdit.value,
+            "firstName": editFirstName.value,
+            "lastName": editLastName.value,
+            "street": editStreet.value,
+        })
+    })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+}
